945ad2d3e2187f1b6bbe576d4d6ef44fd8c2fabc
Дмитрий Дьячков (Dmitri Diakov) <dmitridiakov@gmail.com>
2023-07-20 11:18:25 +0300

    time code and css added

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,94 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+
+import React, { useState, useEffect } from 'react';
+import styles from './page.module.css';
 
 export default function Home() {
+  const [timeInSeconds, setTimeInSeconds] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  function startTimer() {
+    const input = document.getElementById('timerInput');
+    const enteredTime = input.value;
+    const timeArr = enteredTime.split(':');
+
+    if (timeArr.length !== 3) {
+      alert('Please enter time in the format hh:mm:ss');
+      return;
+    }
+
+    const hours = parseInt(timeArr[0]);
+    const minutes = parseInt(timeArr[1]);
+    const seconds = parseInt(timeArr[2]);
+
+    if (isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
+      alert('Please enter valid numbers for hours, minutes, and seconds');
+      return;
+    }
+
+    const totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
+    setTimeInSeconds(totalTimeInSeconds);
+    setRunning(true);
+  }
+
+  function stopTimer() {
+    setRunning(false);
+  }
+
+  function resetTimer() {
+    setTimeInSeconds(0);
+    setRunning(false);
+  }
+
+  function resumeTimer() {
+    setRunning(true);
+  }
+
+  useEffect(() => {
+    if (running && timeInSeconds > 0) {
+      const interval = setInterval(() => {
+        setTimeInSeconds((prevTime) => prevTime - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [running, timeInSeconds]);
+
+  function formatTime(timeInSeconds) {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+  }
+
+  function padZero(num) {
+    return num.toString().padStart(2, '0');
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to Timer <a href="https://github.com/dimkadi">by Dmitri!</a>
+        </h1>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <input type="text" id="timerInput" placeholder="hh:mm:ss" maxLength="8" />
+          {running ? (
+            <>
+              <button onClick={stopTimer}>Stop</button>
+              <button onClick={resetTimer}>Reset</button>
+            </>
+          ) : (
+            <>
+              <button onClick={startTimer}>Start</button>
+            </>
+          )}
+          {!running && timeInSeconds > 0 && <button onClick={resumeTimer}>Resume</button>}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div id="timerDisplay">{formatTime(timeInSeconds)}</div>
+      </main>
+    </>
+  );
 }
